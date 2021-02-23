54c627691fdf9629f732836ef72bb3c8c84c28dd
Gabriel <knuckles4x@gmail.com>
2021-02-23 01:31:36 -0300

    feat: Home section D layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,10 +6,11 @@ import { HomeComponent } from './home.component';
 import { SectionAComponent } from './section-a/section-a.component';
 import { SectionBComponent } from './section-b/section-b.component';
 import { SectionCComponent } from './section-c/section-c.component';
+import { SectionDComponent } from './section-d/section-d.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, SectionAComponent, SectionBComponent, SectionCComponent],
+  declarations: [HomeComponent, SectionAComponent, SectionBComponent, SectionCComponent, SectionDComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
