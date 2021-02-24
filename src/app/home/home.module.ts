abcf1999d60fd9af73e51cd586c7eb4cf1025fce
Gabriel <knuckles4x@gmail.com>
2021-02-24 02:56:33 -0300

    feat get register data from form

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -19,6 +20,6 @@ import { SharedModule } from '../shared/shared.module';
     SectionDComponent,
     SectionEComponent,
   ],
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class HomeModule {}
--- src/app/home/section-a/section-a.component.ts
+++ src/app/home/section-a/section-a.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from 'src/app/shared/validators/must-match.validator';
 
 @Component({
   selector: 'home-section-a',
@@ -6,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-a.component.scss'],
 })
 export class SectionAComponent implements OnInit {
-  constructor() {}
+  arrowColor = '#fff';
+  arrowColorDisabled = '#9b9b9b';
+  registerForm: FormGroup;
 
-  arrowColor = '#9b9b9b';
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group(
+      {
+        cpf: ['', [Validators.required, Validators.minLength(11)]],
+        login: ['', Validators.required],
+        nome: ['', Validators.required],
+        senha: ['', Validators.required],
+        confirmarSenha: ['', Validators.required],
+      },
+      { validator: MustMatch('senha', 'confirmarSenha') }
+    );
+  }
+
+  showError(control: string): boolean {
+    if (!this.registerForm.get(control)) {
+      return false;
+    }
+
+    return (
+      this.registerForm.get(control).invalid &&
+      this.registerForm.get(control).touched
+    );
+  }
+
+  validateAllFormFields() {
+    Object.keys(this.registerForm.controls).forEach((field) => {
+      const control = this.registerForm.get(field);
+      control.markAsTouched();
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      this.validateAllFormFields();
+      return;
+    }
+    console.log(this.registerForm);
+  }
 }
