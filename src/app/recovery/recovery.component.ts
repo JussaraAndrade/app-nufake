2cf24f6b5932925462ae6afed2198177fee8e4ed
Gabriel <knuckles4x@gmail.com>
2021-02-25 18:05:09 -0300

    feat: get recovery and change password data from forms

--- src/app/recovery/recovery.component.ts
+++ src/app/recovery/recovery.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MustMatch } from '../shared/validators/must-match.validator';
 
 @Component({
   selector: 'app-recovery',
@@ -10,14 +17,75 @@ export class RecoveryComponent implements OnInit {
   LOGO_IMAGE_URL = './assets/gama-academy-logo-horizontal-verde-branco1 1.png';
   VECTOR_ICON_URL = './assets/Vector.png';
 
+  forgotPasswordForm: FormGroup;
+  changePasswordForm: FormGroup;
   temporaryPassword: string;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     const paramPassword = this.route.snapshot.paramMap.get('temporaryPassword');
     if (paramPassword) {
       this.temporaryPassword = paramPassword;
+      this.changePasswordForm = this.formBuilder.group(
+        {
+          usuario: ['', Validators.required],
+          senha: ['', Validators.required],
+          confirmarSenha: ['', Validators.required],
+        },
+        { validator: MustMatch('senha', 'confirmarSenha') }
+      );
+    } else {
+      this.forgotPasswordForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        login: ['', Validators.required],
+      });
     }
+  }
+
+  showError(control: string, form: FormGroup): boolean {
+    if (!form.get(control)) {
+      return false;
+    }
+
+    return form.get(control).invalid && form.get(control).touched;
+  }
+
+  validateAllFormFields(form: FormGroup) {
+    Object.keys(form.controls).forEach((field) => {
+      const control = form.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched();
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
+
+  onForgotPasswordSubmit() {
+    if (this.forgotPasswordForm.invalid) {
+      this.validateAllFormFields(this.forgotPasswordForm);
+      return;
+    }
+    this.requestNewTemporaryPassword();
+  }
+
+  requestNewTemporaryPassword() {
+    console.log('pegar uma senha temporaria na api');
+  }
+
+  onChangePasswordSubmit() {
+    if (this.changePasswordForm.invalid) {
+      this.validateAllFormFields(this.changePasswordForm);
+      return;
+    }
+    this.changePassword();
+  }
+
+  changePassword() {
+    console.log('trocar a senha com a api');
   }
 }
--- src/app/recovery/recovery.module.ts
+++ src/app/recovery/recovery.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RecoveryRoutingModule } from './recovery-routing.module';
 import { RecoveryComponent } from './recovery.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RecoveryComponent],
-  imports: [CommonModule, RecoveryRoutingModule],
+  imports: [CommonModule, RecoveryRoutingModule, ReactiveFormsModule],
 })
 export class RecoveryModule {}
