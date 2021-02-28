567708d6f8cd5a702d111abe91fdf4ca37346662
Gabriel <knuckles4x@gmail.com>
2021-02-28 19:01:34 -0300

    feat: internationalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageErroComponent } from './error/error.component';
-import { RecoveryComponent } from './recovery/recovery.component';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
+registerLocaleData(localePt, 'pt');
 @NgModule({
   declarations: [AppComponent, PageErroComponent],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
@@ -25,6 +27,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true,
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt',
     },
   ],
   bootstrap: [AppComponent],
