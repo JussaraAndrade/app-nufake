874675c5ce985f2b6b2a773dcfb08b56085edec3
Gabriel <knuckles4x@gmail.com>
2021-02-28 04:02:53 -0300

    feat: transfer layout, service and interface

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -8,13 +8,17 @@ import { DepositComponent } from './deposit/deposit.component';
 import { PaymentsComponent } from './payments/payments.component';
 import { PlansComponent } from './plans/plans.component';
 import { TransferComponent } from './transfer/transfer.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [DashboardComponent, NavbarComponent, DepositComponent, PaymentsComponent, PlansComponent, TransferComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [
+    DashboardComponent,
+    NavbarComponent,
+    DepositComponent,
+    PaymentsComponent,
+    PlansComponent,
+    TransferComponent,
+  ],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule],
 })
-export class DashboardModule { }
+export class DashboardModule {}
